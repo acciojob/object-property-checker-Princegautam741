6586479218a6e7a1464fa6d4f9125897c179f3eb
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-10-11 16:05:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
-
-function hasKey(key) {
   return key in sampleObject;
 }
 
-// Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
-
-}
-
-// Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
+// Examples
+console.log(hasKey("red"));    // true
+console.log(hasKey("blue"));   // false
+console.log(hasKey("white"));  // true
